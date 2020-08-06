15ea10ccf6737b3e23bfde191d29ea0770746e5d
schaller <benedict-vincent.schaller@adesso.de>
2020-08-06 14:06:09 +0200

    regex for HeroCard V2

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -69,6 +69,7 @@ namespace Microsoft.BotBuilderSamples.Bots
 
             heroCard.Title = title;
             heroCard.Text = answer;
+            heroCard.Buttons.Add(new CardAction());
 
             return heroCard;
         }
