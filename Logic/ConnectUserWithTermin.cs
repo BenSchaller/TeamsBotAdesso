d58d1f2e682042f667a24c5bc4fe55564917e705
schaller <benedict-vincent.schaller@adesso.de>
2020-10-01 15:03:48 +0200

    Bot changes

---
+++ Logic/ConnectUserWithTermin.cs
@@ -0,0 +1,22 @@
+﻿using AdaptiveCards;
+using EchoBot.DatabaseAccess;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EchoBot.Logic
+{
+    public class ConnectUserWithTermin
+    {
+        public void WriteInConnectionTable(TeamsChannelAccount user)
+        {
+            string userId = user.Id;
+            var dbConnection = new DatabaseConnection();
+            dbConnection.InsertIntoConnectionTable(userId);
+        }
+
+    }
+}
